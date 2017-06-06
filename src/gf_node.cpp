fdca78e2cb91361e9593948a3c6eb2b526f59d05
Robin D. Knight <robin.knight@roadnarrows.com>
2017-06-06 14:58:36 -0600

    added geo and distance feature collection processing and publishing

--- src/gf_node.cpp
+++ src/gf_node.cpp
@@ -49,7 +49,6 @@
 
 #include "swri_transform_util/transform_util.h"
 
-
 extern "C" 
 {
   char *ambient_fences_geojson_zoom(double lng,
@@ -67,6 +66,10 @@ extern "C"
 
 namespace geofrenzy
 {
+  typedef std::map<uint64_t, Json::Value> EntitlementMap;
+
+  const double  NoGeoPos = -1000.0;
+
   /**
    * this class implements the GeoJson Fence Server
    */
@@ -87,7 +90,8 @@ namespace geofrenzy
       typedef std::map<std::string, ros::Subscriber> MapSubscriptions;
 
       FenceServer(uint64_t new_class, ros::NodeHandle newnh, double hz) :
-          m_fence_class(new_class), m_nh(newnh), m_hz(hz)
+          m_fence_class(new_class), m_nh(newnh), m_hz(hz),
+          m_access_time(0.0)
       {
         /**
          * this metthod instantiates a fence server
@@ -102,6 +106,12 @@ namespace geofrenzy
         //RDK                           fence_class_stream.str() +
         //RDK                           "/featureCollection/json";
         //RDK metadata_pub = newnh.advertise<std_msgs::String>(fc_string, 1, true);
+        // current and previous positions are unknown
+        m_previous_lat  = NoGeoPos;
+        m_previous_long = NoGeoPos;
+        m_current_lat   = NoGeoPos;
+        m_current_long  = NoGeoPos;
+
         m_nPubRepeatCnt = 0;
       };
   
@@ -206,6 +216,12 @@ namespace geofrenzy
           strPub = ss.str() + "json";
           m_publishers[strPub].publish(m_msgFcJson);
 
+          strPub = ss.str() + "geo";
+          m_publishers[strPub].publish(m_msgFcGeo);
+
+          strPub = ss.str() + "distance";
+          m_publishers[strPub].publish(m_msgFcDist);
+
           --m_nPubRepeatCnt;
         }
       }
@@ -229,55 +245,22 @@ namespace geofrenzy
             1, true);
       }
   
-      void cbNavSatFix(const sensor_msgs::NavSatFix::ConstPtr &msg)
+
+      /*!
+       * \brief Retrieve Geofrenzy Portal fence and entitlements.
+       *
+       * \param [in] latitude   Geographic latitude.
+       * \param [in] longitude  Geographic longitude.
+       * \param [in] level      ??
+       * \param [out] strJson   Json formatted result string.
+       *
+       * \return Returns true on success, false otherwise.
+       */
+      bool retrieveGfPortalFences(double latitude, double longitude,
+                                  int level, std::string &strJson)
       {
-        const char    *topic = "fix";
-
-        ROS_DEBUG_STREAM(m_nh.getNamespace() << "/" << topic);
-
-        Json::Value   root; // will contains the root value after parsing.
-        Json::Reader  reader;
-
-        ROS_INFO_STREAM("Previous lat,long: "
-            << m_previous_lat
-            << ", "
-            << m_previous_long);
-
-        // no gps acquired
-        if (msg->status.status == -1)
-        {
-          ROS_INFO_STREAM("No GPS acquired.");
-          return;
-        }
-  
-        ROS_INFO_STREAM("New lat,long: "
-            << msg->latitude
-            << ", "
-            << msg->longitude);
-  
-        double distance;
-  
-        distance = swri_transform_util::GreatCircleDistance(
-                              msg->latitude, msg->longitude,
-                              m_previous_lat, m_previous_long);
-
-        ROS_DEBUG_STREAM("Distance = " << distance);
-  
-        // ignore this for now, testing
-        if (distance < 0)
-        {
-          return;
-        }
-        else
-        {
-          m_previous_lat  = msg->latitude;
-          m_previous_long = msg->longitude;
-        }
-  
-        ROS_DEBUG_STREAM("Ready to check params.");
-
         std::string filename;
-        char *t;
+        char        *s;
   
         if (m_nh.getParam("geojson_file", filename))
         {
@@ -286,15 +269,13 @@ namespace geofrenzy
           std::ifstream in(filename.c_str());
           std::string message;
   
-          while (in)
+          while(in)
           {
             message.push_back(in.get());
           }
   
-          char *bt = &message[0u];
-          t = bt;
-          //std::vector<char> buf(message.c_str(), message.c_str() + message.size() + 1);
-          //t = buf;
+          s = &message[0u];
+
           ROS_DEBUG_STREAM("Done reading file.");
         }
 
@@ -302,63 +283,122 @@ namespace geofrenzy
         {
           ROS_DEBUG_STREAM("Checking fence roi for class " << m_fence_class);
 
-          ROS_DEBUG_STREAM("Calling ambient_fences_geojson_roi().");
+          ROS_DEBUG_STREAM("Calling ambient_fences_geojson_roi()");
 
-          char *td = ambient_fences_geojson_roi(msg->longitude, msg->latitude,
-                                                6, m_fence_class);
+          s = ambient_fences_geojson_roi(longitude, latitude, level,
+                                         m_fence_class);
 
           ROS_DEBUG_STREAM("Done.");
-          t = td;
         }
   
-        ROS_DEBUG_STREAM("begin\n" << t << "end");
+        if( s != NULL )
+        {
+          strJson = s;
+          m_access_time = ros::Time::now();
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+      }
 
-        std::stringstream t_stream;
-        t_stream << t;
+    protected:
 
-        updateFeatureCollectionMsgs(t);
-        //RDK std_msgs::String fence_message;
-        //RDK fence_message.data = t_stream.str();
-        //RDK metadata_pub.publish(fence_message);
-  
-        bool parsingSuccessful = reader.parse(t, root);
-        if (!parsingSuccessful)
+      /*!
+       * \brief Process json encoded Geofrenzy fences.
+       *
+       * \param strJson   Json encoded string.
+       *
+       * \return Returns true on success, false otherwise.
+       */
+      bool processJsonFences(const std::string &strJson)
+      {
+        Json::Reader  reader;   // json reader
+        Json::Value   root;     // will contains the root value after parsing.
+        bool          bSuccess; // [not] successful
+
+        bSuccess = reader.parse(strJson, root);
+
+        if( !bSuccess )
         {
           // report to the user the failure and their locations in the document.
           ROS_ERROR_STREAM("Failed to parse fences.\n"
                                 << reader.getFormattedErrorMessages());
-          return;
+          return false;
         }
   
-        std::map <uint64_t, Json::Value> entitlement_temp_map;
-        std::map<uint64_t, bool> inorout;
-        Json::Value FeatureList = root["features"];
+        EntitlementMap            entitlement_temp_map;
+        std::map<uint64_t, bool>  inorout;
 
-        for (Json::Value::iterator feature = FeatureList.begin();
-            feature != FeatureList.end();
+        Json::Value featureList = root["features"];
+
+        m_msgFcGeo.features.clear();
+
+        GfGeoFeature  msgGeoFeature;
+        GeoPolygon    msgGeoPolygon;
+
+        for(Json::Value::iterator feature = featureList.begin();
+            feature != featureList.end();
             ++feature)
         {
-          Json::Value Entitlements = (*feature)["properties"]["class_metadata"]["entitlements"];
-          for (Json::Value::iterator entitlement = Entitlements.begin();
-               entitlement != Entitlements.end();
-               ++entitlement)
+          msgGeoFeature.gf_class_idx = m_fence_class;
+          msgGeoFeature.gf_ent_idx.clear();
+          msgGeoFeature.geometry.clear();
+
+          Json::Value geometry = (*feature)["geometry"]["coordinates"];
+
+          for(Json::Value::iterator polygon_iter = geometry.begin();
+              polygon_iter != geometry.end();
+              ++polygon_iter)
           {
-            uint64_t ent_idx = (*entitlement)["ent_idx"].asInt();
+            Json::Value &polygon = (*polygon_iter);
+
+            msgGeoPolygon.points.clear();
+
+            for(Json::Value::iterator point_iter = polygon.begin();
+                point_iter != polygon.end();
+                ++point_iter)
+            {
+              geographic_msgs::GeoPoint msgGeoPoint;
+
+              msgGeoPoint.longitude = (*point_iter)[0].asDouble();
+              msgGeoPoint.latitude  = (*point_iter)[1].asDouble();
+              msgGeoPoint.altitude  = 0.0;
+
+              msgGeoPolygon.points.push_back(msgGeoPoint);
+            }
+          }
+
+          msgGeoFeature.geometry.push_back(msgGeoPolygon);
+
+          Json::Value entitlements =
+                    (*feature)["properties"]["class_metadata"]["entitlements"];
+
+          for(Json::Value::iterator entitlement = entitlements.begin();
+              entitlement != entitlements.end();
+              ++entitlement)
+          {
+            uint64_t          ent_idx = (*entitlement)["ent_idx"].asInt();
+            Json::FastWriter  fastWriter;
+
+            msgGeoFeature.gf_ent_idx.push_back(ent_idx);
+
             entitlement_temp_map[ent_idx] = *entitlement;
-            Json::FastWriter fastWriter;
 
             std::string entitlement_string = fastWriter.write(*entitlement);
 
-            Json::Value Inout = (*feature)["properties"]["inout"];
+            Json::Value inout = (*feature)["properties"]["inout"];
 
             ROS_DEBUG_STREAM("entitlement start\n"
                 << entitlement_string << "\n"
                 << ent_idx << "\n"
                 << "entitlement end");
 
-            ROS_DEBUG_STREAM("inout = " << Inout);
+            ROS_DEBUG_STREAM("inout = " << inout);
   
-            if (Inout.asString().compare("i") == 0)
+            // RDK bit-or bool??
+            if( inout.asString().compare("i") == 0 )
             {
               inorout[ent_idx] = inorout[ent_idx] | true;
             }
@@ -368,34 +408,170 @@ namespace geofrenzy
             }
           }
   
-          for (std::map<uint64_t, Json::Value>::iterator val_it = entitlement_temp_map.begin();
+          for(EntitlementMap::iterator val_it = entitlement_temp_map.begin();
               val_it != entitlement_temp_map.end();
               ++val_it)
           {
-            uint64_t tempidx = val_it->first;
+            uint64_t          tempidx = val_it->first;
+            Json::FastWriter  fastWriter;
+
             val_it->second["dwell"] = inorout[tempidx];
+
             std::stringstream fence_class_str;
             fence_class_str << m_fence_class;
             val_it->second["class_idx"] = fence_class_str.str();
-            Json::FastWriter fastWriter;
-            std::string entitlement_temp_string = fastWriter.write(val_it->second);
+
+            std::string entitlement_temp_string =
+                                              fastWriter.write(val_it->second);
+
             std_msgs::String entitlement_message;
             entitlement_message.data = entitlement_temp_string;
             //entitlement_message.header.stamp = ros::Time::now();
             entitlement_map[tempidx].publish(entitlement_message);
             // dwell_pub.publish(entitlement_message);
           }
-        };
+
+          m_msgFcGeo.features.push_back(msgGeoFeature);
+        }
+
+        return true;
+      }
+
+      void convertGeoToDistMsg()
+      {
+        m_msgFcDist.features.clear();
+
+        GfDistFeature msgDistFeature;
+        Polygon64     msgDistPolygon;
+
+        for(size_t i = 0; i < m_msgFcGeo.features.size(); ++i)
+        {
+          GfGeoFeature &msgGeoFeature = m_msgFcGeo.features[i];
+
+          msgDistFeature.gf_class_idx = msgGeoFeature.gf_class_idx;
+          msgDistFeature.gf_ent_idx.clear();
+          msgDistFeature.geometry.clear();
+
+          for(size_t j = 0; j < msgGeoFeature.gf_ent_idx.size(); ++j)
+          {
+            msgDistFeature.gf_ent_idx.push_back(msgGeoFeature.gf_ent_idx[j]);
+          }
+
+          for(size_t j = 0; j < msgGeoFeature.geometry.size(); ++j)
+          {
+            GeoPolygon &msgGeoPolygon = msgGeoFeature.geometry[j];
+
+            msgDistPolygon.points.clear();
+
+            for(size_t k = 0; k < msgGeoPolygon.points.size(); ++k)
+            {
+              geographic_msgs::GeoPoint &msgGeoPoint = msgGeoPolygon.points[k];
+              geometry_msgs::Point      msgPoint;
+
+              swri_transform_util::LocalXyFromWgs84(
+                        m_current_lat, m_current_long,
+                        msgGeoPoint.latitude, msgGeoPoint.longitude,
+                        msgPoint.x, msgPoint.y);
+
+              msgPoint.z  = msgGeoPoint.altitude;
+
+              msgDistPolygon.points.push_back(msgPoint);
+            }
+
+            msgDistFeature.geometry.push_back(msgDistPolygon);
+          }
+
+          m_msgFcDist.features.push_back(msgDistFeature);
+        }
+      }
+
+      /*!
+       * \brief Navigation satellite fix callback.
+       */
+      void cbNavSatFix(const sensor_msgs::NavSatFix::ConstPtr &msg)
+      {
+        const char    *topic = "fix";
+
+        ROS_DEBUG_STREAM(m_nh.getNamespace() << "/" << topic);
+
+        std::string   strJson;
+
+        // no gps acquired
+        if( msg->status.status == -1 )
+        {
+          ROS_INFO_STREAM("No GPS acquired.");
+          return;
+        }
+  
+        m_previous_lat  = m_current_lat;
+        m_previous_long = m_current_long;
+        m_current_lat   = msg->latitude;
+        m_current_long  = msg->longitude;
+
+        // no previous - set to current
+        if( (m_previous_lat <= NoGeoPos) || (m_previous_long <= NoGeoPos) )
+        {
+          m_previous_lat  = m_current_lat;
+          m_previous_long = m_current_long;
+        }
+
+        ROS_INFO_STREAM("Previous lat,long: "
+            << m_previous_lat
+            << ", "
+            << m_previous_long);
+
+        ROS_INFO_STREAM("     New lat,long: "
+            << m_current_lat
+            << ", "
+            << m_current_long);
+  
+        double distance;
+  
+        distance = swri_transform_util::GreatCircleDistance(
+                              m_current_lat, m_current_long,
+                              m_previous_lat, m_previous_long);
+
+        ROS_DEBUG_STREAM("Distance = " << distance);
+  
+        // ignore this for now, testing
+        // RDK future parameter db value
+        if( distance < 0 )
+        {
+          return;
+        }
+  
+        // RDK 6 is future parameter db value
+        if( !retrieveGfPortalFences(m_current_lat, m_current_long, 6, strJson) )
+        {
+          ROS_ERROR("Failed to retrieve Geofrenzy fences.");
+          return;
+        }
+
+        if( !processJsonFences(strJson) )
+        {
+          ROS_ERROR("Failed to process Geofrenzy fences.");
+          return;
+        }
+
+        updateFeatureCollectionMsgs(strJson);
       };
 
-
-    protected:
       /*!
        * \brief Update feature collection messages from current location.
        */
-      void updateFeatureCollectionMsgs(std::string strJson)
+      void updateFeatureCollectionMsgs(const std::string &strJson)
       {
-        m_msgFcJson.data = strJson;
+        m_msgFcJson.data        = strJson;
+        //m_msgFcJson.features.data  = strJson;
+        //m_msgFcJson.access_time  = m_access_time;
+        //stampHeader(m_msgFcJson.header, m_msgFcJson.header.seq+1);
+
+        m_msgFcGeo.access_time = m_access_time;
+        stampHeader(m_msgFcGeo.header, m_msgFcGeo.header.seq+1);
+
+        convertGeoToDistMsg();
+        m_msgFcDist.access_time = m_access_time;
+        stampHeader(m_msgFcDist.header, m_msgFcDist.header.seq+1);
 
         ++m_nPubRepeatCnt;
       }
@@ -427,13 +603,17 @@ namespace geofrenzy
       MapPublishers     m_publishers;     ///< Laelaps control publishers
       MapSubscriptions  m_subscriptions;  ///< Laelaps control subscriptions
 
-      double_t m_previous_lat;
-      double_t m_previous_long;
+      ros::Time m_access_time;
+      double    m_previous_lat;
+      double    m_previous_long;
+      double    m_current_lat;
+      double    m_current_long;
 
       // Message for publishing
-      std_msgs::String        m_msgFcJson;
       GfGeoFeatureCollection  m_msgFcGeo;
       GfDistFeatureCollection m_msgFcDist;
+      std_msgs::String        m_msgFcJson;    // RDK deprecate
+      //GfJsonFeatureCollection m_msgFcJson;  // RDK and replace with this
       int                     m_nPubRepeatCnt;
 
       //RDK ros::Publisher metadata_pub;
@@ -726,7 +906,7 @@ int main(int argc, char **argv)
     // make any callbacks on pending ROS events
     ros::spinOnce(); 
 
-    // publish all advertized topics
+    // publish all advertised topics
     fs.publish();
 
     // sleep to keep at loop rate
