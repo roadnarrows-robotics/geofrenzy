319fb680743af99509a7fe5b872849816062e387
Robin D. Knight <robin.knight@roadnarrows.com>
2017-06-14 11:20:15 -0600

    add gf_math.* file blanks to be used for geometry calcs.

---
+++ src/gf_math.cpp
@@ -0,0 +1,16 @@
+// GEOFRENZY FILE HEADER HERE
+
+#include <stdio.h>
+#include <string>
+
+#include "gf_math.h"
+
+using namespace std;
+
+namespace geofrenzy
+{
+  namespace gf_math
+  {
+
+  } // namespace gf_math
+} // namespace geofrenzy
---
+++ src/gf_math.h
@@ -0,0 +1,19 @@
+// GEOFRENZY FILE HEADER HERE
+
+#ifndef _GF_MATH_H
+#define _GF_MATH_H
+
+#include <string>
+
+namespace geofrenzy
+{
+  namespace gf_math
+  {
+    // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+    // Special values
+    // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+ 
+  } // namespace gf_math
+} // namespace geofrenzy
+
+#endif // _GF_MATH_H
