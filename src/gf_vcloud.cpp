1fe3a190a60c54de375e77901f1f5723b4478809
Robin D. Knight <robin.knight@roadnarrows.com>
2017-09-26 06:35:45 -0700

    forgot to turn off unit testing

--- src/gf_vcloud.cpp
+++ src/gf_vcloud.cpp
@@ -92,7 +92,7 @@ using namespace geofrenzy::gf_scene;
 // Unit Test Switch (only available if math ut is also enabled)  
 //
 #ifdef GF_SCENE_UT
-#define GF_VCLOUD_NODE_UT  ///< define to enable unit test
+#undef GF_VCLOUD_NODE_UT  ///< define to enable unit test
 #else
 #undef GF_VCLOUD_NODE_UT  ///< ut always disabled
 #endif // GF_SCENE_UT
